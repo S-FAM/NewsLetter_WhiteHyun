b4f969dbfa4fe99be4d95c0e3c735ab5d74eb1ce
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-06-10 23:07:07 +0900

    Feat: 아이콘 이미지 뷰 추가

--- NewsBee/Source/ViewControllers/HomeViewController.swift
+++ NewsBee/Source/ViewControllers/HomeViewController.swift
@@ -9,8 +9,39 @@ import UIKit
 
 class HomeViewController: UIViewController {
   
+  
+  /// 상단에 보이는 뉴스비 아이콘 이미지입니다.
+  private let iconImageView: UIImageView = {
+    let image = UIImage(named: "mainIcon")
+    let imageView = UIImageView(image: image)
+    imageView.tintColor = .label
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
+    
+    setupLayouts()
+    setupConstraints()
+  }
+  
+  func setupLayouts() {
+    view.addSubview(iconImageView)
+  }
+  
+  func setupConstraints() {
+
+    
+    // MARK: IconImage
+    NSLayoutConstraint.activate(
+      [
+        iconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        iconImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+        iconImageView.widthAnchor.constraint(equalToConstant: 80),
+        iconImageView.heightAnchor.constraint(equalToConstant: 80)
+      ]
+    )
   }
 }
