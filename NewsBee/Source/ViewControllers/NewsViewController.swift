2f2263fbe628f5bc454ef6eb1d8b4bd0fa76db08
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-06-10 23:06:16 +0900

    Feat: 뉴스VC 생성

---
+++ NewsBee/Source/ViewControllers/NewsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViewController.swift
+//  NewsBee
+//
+//  Created by 홍승현 on 2022/06/10.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+  
+}
