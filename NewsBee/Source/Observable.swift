1778360a84ae4098eb4fc262bd12fae9b1a20de4
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-06-16 23:51:33 +0900

    Feat: Observable optional 제거

--- NewsBee/Source/Observable.swift
+++ NewsBee/Source/Observable.swift
@@ -7,19 +7,19 @@
 
 class Observable<T> {
   
-  var value: T? {
+  var value: T {
     didSet {
       listener?(value)
     }
   }
   
-  init(_ value: T?) {
+  init(_ value: T) {
     self.value = value
   }
   
-  private var listener: ((T?) -> Void)?
+  private var listener: ((T) -> Void)?
   
-  func bind(_ listener: @escaping (T?) -> Void) {
+  func bind(_ listener: @escaping (T) -> Void) {
     listener(value)
     self.listener = listener
   }
