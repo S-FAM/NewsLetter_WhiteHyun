9a1aa37d4c088108714b2d88cea844af06efeb3a
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-06-23 18:01:20 +0900

    Feat: 서버로부터 가져올 Codable 구조체 구현

---
+++ NewsBee/Source/Models/MyNewsInfo.swift
@@ -0,0 +1,38 @@
+//
+//  MyNewsInfo.swift
+//  NewsBee
+//
+//  Created by 홍승현 on 2022/06/23.
+//
+
+import Foundation
+
+
+struct MyNewsInfo: Codable {
+  
+  /// 뉴스 제목
+  let title: String
+  
+  /// 뉴스의 내용 요약
+  let description: String
+  
+  /// 뉴스 이미지
+  let imageLink: String?
+  
+  /// 언론사 하이퍼텍스트 link
+  let url: String
+  
+  /// 뉴스 출판 시간
+  let publishDate: String
+  
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case title
+    case description
+    case url
+    case imageLink = "image"
+    case publishDate = "date"
+  }
+  
+}
